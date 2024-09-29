2235006fde746f0196b282cbb3ca3ab36e130857
Nataliia Medvedeva <shishika87@gmail.com>
2024-09-29 21:27:47 +0700

    Commit2

--- src/test/java/ru/netology/web1/CallbackTest.java
+++ src/test/java/ru/netology/web1/CallbackTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,7 +23,12 @@ class CallbackTest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -33,7 +39,7 @@ class CallbackTest {
 
     @Test
     void shouldTestCallback() {
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Алексей");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Наталья");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79612535659");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button")).click();
