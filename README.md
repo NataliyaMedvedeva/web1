91abebecd03e413e0684371c175b46d33c831a7a
Nataliia Medvedeva <shishika87@gmail.com>
2024-09-29 21:13:10 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![example workflow](https://github.com/NataliyaMedvedeva/web1/actions/workflows/main.yml/badge.svg)
---
+++ src/test/java/ru/netology/web1/CallbackTest.java
@@ -0,0 +1,46 @@
+package ru.netology.web1;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CallbackTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestCallback() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Алексей");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79612535659");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+
+        assertEquals(expected, actual);
+    }
+}
